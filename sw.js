38758bd90059c86be28ef812e4b90ecabb2d57fe
Yussuf254 <yusufhamisi00@gmail.com>
2025-05-25 21:46:00 +0300

    initialized project

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'budalangi-poll-v1';
+const CACHE_URLS = [
+    '/',
+    '/BudalangiOpinionPoll.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(CACHE_URLS);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then((response) => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+}); 
